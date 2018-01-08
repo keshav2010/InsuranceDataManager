929521312e26ab4b049e5cde73ce8c222969ffd1
keshav2010 <sharmakeshav15157@gmail.com>
2018-01-09 00:39:34 +0530

    updated CustomDateField, small issue with positioning in form

--- src/DisplayForms/AddRecordDialog.java
+++ src/DisplayForms/AddRecordDialog.java
@@ -1,17 +1,25 @@
 package DisplayForms;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 //Add Record Dialog class, this dialog is poped up when user decides to add a new record to a file, 
 //The dialog contains multiple fields for user to enter record's information and save it to file.
 public class AddRecordDialog extends JDialog{
@@ -21,7 +29,7 @@ public class AddRecordDialog extends JDialog{
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		this.setTitle("Add New Record");
 		this.setLayout(new GridBagLayout());
-		this.setSize(350, 350);
+		this.setSize(500, 350);
 		setupForm(); //sets up the form
 		this.setVisible(true);
 	}
@@ -47,9 +55,12 @@ public class AddRecordDialog extends JDialog{
 				"D.O.B", "D.O.C", "Sum Assured", "Plan And Term",
 				"Mode", "Primium", "Next Due"};
 		
-		for(int i=0; i<labels.length; i++)
-			addTextField(labels[i], i);
-		
+		for(int i=0; i<labels.length; i++) {
+			if(labels[i].equals("D.O.B") || labels[i].equals("D.O.C")) {
+				addDateField(labels[i], i);
+			}
+			else addTextField(labels[i], i);
+		}		
 		//setting up GridBagConstraints properties for submit-button in the form
 		updateLayoutHandler(0, labels.length, GridBagConstraints.HORIZONTAL, 0, 0);
 		dialogLayoutHandler.gridwidth=2;
@@ -62,7 +73,6 @@ public class AddRecordDialog extends JDialog{
 		JLabel textLabel = new JLabel(labelName);
 		updateLayoutHandler(0, rowNumber, GridBagConstraints.HORIZONTAL, 1, 1);
 		dialogLayoutHandler.insets.left = 15; //some custom changes
-		
 		this.add(textLabel, dialogLayoutHandler);
 		JTextField textData = new JTextField();
 		textData.setEditable(true);
@@ -72,17 +82,57 @@ public class AddRecordDialog extends JDialog{
 		
 		this.add(textData, dialogLayoutHandler);
 	}
+	
+	//helper function : adds a custom dateField
+	private void addDateField(String labelName, int rowNumber) {
+		JLabel textLabel = new JLabel(labelName);
+		updateLayoutHandler(0, rowNumber, GridBagConstraints.HORIZONTAL, 1, 1);
+		dialogLayoutHandler.insets.left= 15;
+		this.add(textLabel, dialogLayoutHandler);
+		
+		CustomDateField dateField = new CustomDateField();
+		
+		updateLayoutHandler(0, rowNumber, GridBagConstraints.HORIZONTAL, 1, 1);
+		dialogLayoutHandler.insets.right= 15;
+		dialogLayoutHandler.gridwidth=3;
+		this.add(dateField, dialogLayoutHandler);
+	}
+	
 }
 
 //helper class, sets up a basic date field 
-class CustomDateField  extends Component// DD- MM - YYYY (DateFormat Predominant in India)
+class CustomDateField  extends JComponent implements SwingConstants
 {
 	private Date date;
-	private JComboBox monthBox;
+	private JComboBox<String> monthBox;
 	private JTextField dateField;
 	private JTextField yearField;
+	FlowLayout layout = new FlowLayout();
 	public CustomDateField() {
 		
+		String[] monthsList = { "JAN" , "FEB", "MAR",
+				"APR", "MAY", "JUNE", 
+				"JULY", "AUG", "SEPT", 
+				"OCT", "NOV", "DEC"};	
+		
+		monthBox = new JComboBox<String>(monthsList);
+		monthBox.setSelectedIndex(0);
+		monthBox.setEditable(false);
+
+		dateField = new JTextField("01");
+		yearField = new JTextField("2018");
+				
+		layout.setHgap(2);
+		layout.setAlignment(FlowLayout.TRAILING);
+		this.setLayout(layout);
+		
+		this.add(dateField);
+		this.add(monthBox);
+		this.add(yearField);
+		date = new Date(dateField.getText(), monthBox.getSelectedItem().toString(), yearField.getText());
+	}
+	public Date getDate() {
+		return date;
 	}
 }
 
@@ -99,6 +149,7 @@ class Date{
 	public String getDateString() {
 		return new String(day + month + year);
 	}
+	
 	public void setDate(String day, String month, String year) {
 		this.day = new String(day);
 		this.month = new String(month);
